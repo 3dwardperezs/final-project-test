3ab836fcc446d3ab7b3d2d138ccc0bd60a3b66ef
3dwardperezs <3dwardperezs@gmail.com>
2024-08-04 22:36:52 +0100

    new changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [
@@ -10,4 +11,5 @@ export default defineConfig({
   output: "server",
   site: 'https://3dwardperezs.github.io',
   base: 'final-project-test',
+  adapter: netlify(),
 });
